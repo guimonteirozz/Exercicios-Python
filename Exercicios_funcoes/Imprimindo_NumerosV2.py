d05f5d120a75851c60725363d1f8646bd7c205c5
Guilherme Filho <guilhermefilho635@gmail.com>
2023-01-15 18:57:02 -0300

    add comentario
    add comentar descrevendo a questao

--- Exercicios_funcoes/Imprimindo_NumerosV2.py
+++ Exercicios_funcoes/Imprimindo_NumerosV2.py
@@ -1,8 +1,16 @@
 def valorDeN(n):
-  for i in range(n):
+  for i in range(n): # repete a linhas ate a n-enésima linha
     print("\r")
     for i in range(i+1):
-      print(str(i + 1), end=" ")
+      print(str(i + 1), end=" ") # Imprimi os numeros ate o valor de i + 1 em linha reta
       
-n = int(input("Valor de N:"))
+n = int(input("Valor de N: "))
 valorDeN(n)
+
+'''
+  1
+  1   2
+  1   2   3
+  .....
+  1   2   3   ...  n
+'''
